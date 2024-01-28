3a810004b3d39edd71582d205e28d0e58037c0be
h <h>
2024-01-27 21:33:41 -0600

    Use Hopscotch.htn as a prelude to any projects
    all of index.js is still temporary tho
    
    Now dancingbear.htn works without any "_" definitions!

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const {hopscotchify} = require('./hopscotchify.js')
 if (process.argv.length < 3)
 	return console.error(`Nope! Try ${process.argv[0]} ${process.argv[1]} <file.htn>`)
 
-const htnCode = fs.readFileSync(process.argv[2]).toString()
+const prelude = fs.readFileSync(__dirname+"/Hopscotch.htn").toString()
+
+const project = fs.readFileSync(process.argv[2]).toString()
+const htnCode = prelude + "\n" + project
 console.log(JSON.stringify(hopscotchify(htnCode, {checkParameterLabels: true})))
