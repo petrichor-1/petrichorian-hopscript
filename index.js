ab84aa31272cb7fa916141f7afe2fae0f9a67036
h <h>
2024-01-27 22:13:09 -0600

    Output errors even if they arent syntax errors

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ if (process.argv.length < 3)
 	return console.error(`Nope! Try ${process.argv[0]} ${process.argv[1]} <file.htn>`)
 
 const prelude = fs.readFileSync(__dirname+"/Hopscotch.htn").toString()
+const preludeLineCount = prelude.split("\n").length
 
-const project = fs.readFileSync(process.argv[2]).toString()
+const inputPath = process.argv[2]
+const project = fs.readFileSync(inputPath).toString()
 const htnCode = prelude + "\n" + project
-console.log(JSON.stringify(hopscotchify(htnCode, {checkParameterLabels: true})))
+
+try {
+	const hopscotchified = hopscotchify(htnCode, {checkParameterLabels: true})
+	console.log(JSON.stringify(hopscotchified))
+} catch (error) {
+	try {
+		const message = `${error.toString()}\nExpected '${error.expected}', found '${error.found}' at ${inputPath}:${error.location.start.line-preludeLineCount}:${error.location.start.column}`
+		console.log(message)
+	} catch {
+		console.log(error)
+	}
+}
