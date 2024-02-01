a1690fecc75113a133225212b02d74cb49b2fac1
h <h>
2024-01-31 19:31:55 -0600

    Allow including more in the prelude with `#include` at the start of files
    This is temporary

--- compile.js
+++ compile.js
@@ -4,11 +4,34 @@ const {hopscotchify} = require('./core/hopscotchify.js')
 if (process.argv.length < 3)
 	return console.error(`Nope! Try ${process.argv[0]} ${process.argv[1]} <file.htn>`)
 
-const prelude = fs.readFileSync(__dirname+"/core/prelude/Hopscotch.htn").toString()
-const preludeLineCount = prelude.split("\n").length
-
 const inputPath = process.argv[2]
 const project = fs.readFileSync(inputPath).toString()
+const preludePath = __dirname + "/core/prelude/"
+const preludeFiles = [preludePath+"Hopscotch.htn"]
+for (let i = 0; project.split('\n')[i].startsWith("#include "); i++) {
+	const nextFile = project.split('\n')[i].substring("#include ".length)
+	if (/\//.test(nextFile))
+		return console.error(`Invalid include file path ${nextFile}`)
+	const fullPath = preludePath + nextFile
+	if (!fs.existsSync(fullPath))
+		return console.error(`Include file ${nextFile} does not exist`)
+	preludeFiles.push(fullPath)
+}
+const alreadyIncluded = new Set()
+let prelude = ""
+const fileMap = []
+preludeFiles.forEach(path => {
+	if (alreadyIncluded.has(path))
+		return
+	alreadyIncluded.add(path)
+	const fileContents = fs.readFileSync(path).toString()
+	fileMap.push({file: path, starts: prelude.split('\n').length, length: fileContents.split('\n').length})
+	prelude+=fileContents
+	prelude += "\n"
+})
+
+fileMap.push({file: inputPath, starts: prelude.split('\n').length, length: project.split('\n').length})
+
 const htnCode = prelude + "\n" + project
 
 try {
@@ -27,7 +50,17 @@ try {
 				return "end of file"
 			return e
 		}) : [error.expected]
-		const message = `Syntax error: Expected '${expected.filter(e=>!e.startsWith("[internal]")).join("', '")}', found '${error.found}'\n\tat ${inputPath}:${error.location.start.line-preludeLineCount}:${error.location.start.column}`
+		const locationFile = function(){
+			for (let i = 0; i < fileMap.length; i++) {
+				let file = fileMap[i]
+				if (file.starts + file.length >= error.location.start.line)
+					return file
+			}
+			return null
+		}()
+		const file = locationFile.file
+		const lineOffset = locationFile.starts
+		const message = `Syntax error: ${error.message ? error.message : ""} \n\tExpected '${expected.filter(e=>!e.startsWith("[internal]")).join("', '")}', found '${error.found}'\n\tat ${file}:${error.location.start.line-lineOffset}:${error.location.start.column}`
 		console.log(message)
 		const line = htnCode.split('\n')[error.location.start.line-1]
 		const before = line.substring(0,error.location.start.column-1)
@@ -40,7 +73,7 @@ try {
 		for (let i = 0; i < error.location.end.column - error.location.start.column; i++)
 			arrowLine += "^"
 		console.log(arrowLine)
-	} catch {
-		console.log(error)
+	} catch (e2) {
+		console.log(error, e2)
 	}
 }
